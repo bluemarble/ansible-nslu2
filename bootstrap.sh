cc25c97bc1ec70f740231b72f23e8045a101b10b
Paul Steinbach <paul.steinbach@karoshealth.com>
2016-04-08 15:16:08 -0400

    create /etc/unslung-release file

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,4 @@
 
 ansible onyx -i inventory -m raw -a "ipkg install python27" # python-simplejson
 ansible onyx -i inventory -m raw -a "ln -s /opt/bin/python2.7 /usr/bin/python"
+ansible onyx -i inventory -m raw -a "echo V2.3R63-uNSLUng-6.8-beta >/etc/unslung-release"
